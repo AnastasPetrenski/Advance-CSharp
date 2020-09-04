0087cdd35a01bf95fc85be3072e21806b895c2a1
AnastasPetrenski <hloro83@gmail.com>
2020-09-04 17:37:09 +0300

    StreamFilesAndDirectories

---
+++ Advance04StreamsFilesAndDirectories/Advance04StreamsFilesAndDirectories/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Advance04StreamsFilesAndDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileStream file = new FileStream("TextTest.txt");
+            StreamWriter writer = new StreamWriter("result.txt");
+
+            using (writer)
+            {
+                writer.Write("Text");
+            }
+
+            using StreamReader reader = new StreamReader("result.txt");
+
+            string result = reader.ReadLine();
+
+            Console.WriteLine(result);
+
+            using var read = new StreamReader("TextTest.txt");
+            string test = read.ReadLine();
+
+            Console.WriteLine(test);
+        }
+    }
+}
