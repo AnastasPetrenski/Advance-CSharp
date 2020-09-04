162841bc5c0584c2c1731e03da3b9874b9ad8ec7
AnastasPetrenski <hloro83@gmail.com>
2020-09-04 17:34:10 +0300

    Uploading

---
+++ Advance02MultidimensionalArrays/16 Miner/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16_Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> commands = ConvertInputToArray();
+            var startPosition = new KeyValuePair<int, int>();
+            int coals = 0;
+
+            var matrix = new string[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                var input = ConvertInputToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (input[col] == "s")
+                    {
+                        startPosition = new KeyValuePair<int, int>(row, col);
+                    }
+                    else if (input[col] == "c")
+                    {
+                        coals++; 
+                    }
+                }
+            }
+
+            int totalCoals = 0;
+            bool isEndOccurred = false;
+            for (int i = 0; i < commands.Count; i++)
+            {
+                if (commands[i].Equals("up"))
+                {
+                    if (startPosition.Key - 1 >= 0)
+                    {
+                        string letter = matrix[startPosition.Key - 1, startPosition.Value];
+                        if (letter.Equals("c"))
+                        {
+                            totalCoals++;
+                            matrix[startPosition.Key - 1, startPosition.Value] = "*";
+                        }
+                        else if (letter == "e")
+                        {
+                            isEndOccurred = true;
+                            startPosition = new KeyValuePair<int, int>(startPosition.Key - 1, startPosition.Value);
+                            break;
+                        }
+                        startPosition = new KeyValuePair<int, int>(startPosition.Key - 1, startPosition.Value);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "down")
+                {
+                    if (startPosition.Key + 1 < matrix.GetLength(0))
+                    {
+                        string letter = matrix[startPosition.Key + 1, startPosition.Value];
+                        if (letter == "c")
+                        {
+                            totalCoals++;
+                            matrix[startPosition.Key + 1, startPosition.Value] = "*";
+                        }
+                        else if (letter == "e")
+                        {
+                            startPosition = new KeyValuePair<int, int>(startPosition.Key + 1, startPosition.Value);
+                            isEndOccurred = true;
+                            break;
+                        }
+                        startPosition = new KeyValuePair<int, int>(startPosition.Key + 1, startPosition.Value);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "right")
+                {
+                    if (startPosition.Value + 1 < matrix.GetLength(1))
+                    {
+                        string letter = matrix[startPosition.Key, startPosition.Value + 1];
+                        if (letter == "c")
+                        {
+                            totalCoals++;
+                            matrix[startPosition.Key, startPosition.Value + 1] = "*";
+                        }
+                        else if (letter == "e")
+                        {
+                            isEndOccurred = true;
+                            startPosition = new KeyValuePair<int, int>(startPosition.Key, startPosition.Value + 1);
+                            break;
+                        }
+                        startPosition = new KeyValuePair<int, int>(startPosition.Key, startPosition.Value + 1);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (commands[i] == "left")
+                {
+                    if (startPosition.Value - 1 >= 0)
+                    {
+                        string letter = matrix[startPosition.Key, startPosition.Value - 1];
+                        if (letter == "c")
+                        {
+                            totalCoals++;
+                            matrix[startPosition.Key, startPosition.Value - 1] = "*";
+                        }
+                        else if (letter == "e")
+                        {
+                            isEndOccurred = true;
+                            startPosition = new KeyValuePair<int, int>(startPosition.Key, startPosition.Value - 1);
+                            break;
+                        }
+
+                        startPosition = new KeyValuePair<int, int>(startPosition.Key, startPosition.Value - 1);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                if (totalCoals == coals)
+                {
+                    Console.WriteLine($"You collected all coals! ({startPosition.Key}, {startPosition.Value})");
+                    return;
+                }
+            }
+
+            if (isEndOccurred)
+            {
+                Console.WriteLine($"Game over! ({startPosition.Key}, {startPosition.Value})");
+            }
+            else
+            {
+                Console.WriteLine($"{coals - totalCoals} coals left. ({startPosition.Key}, {startPosition.Value})");
+            }
+        }
+
+        public static List<string> ConvertInputToArray()
+            => Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+    }
+}
---
+++ Advance02MultidimensionalArrays/17 RadioactiveMutantVampireBunnies/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17_RadioactiveMutantVampireBunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+            Position playerPosition = new Position(0, 0);
+
+            char[,] gameField = new char[rows, cols];
+            //GenerateGameField(gameField, rows, cols);
+            //playerPosition = PlayerPosition(row, col);
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] input = Console.ReadLine().ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    gameField[row, col] = input[col];
+                    if (input[col] == 'P')
+                    {
+                        playerPosition.Row = row;
+                        playerPosition.Col = col;
+                    }
+                }
+            }
+
+            bool isDead = false;
+            bool escapedSuccessfully = false;
+            char[] commands = Console.ReadLine().ToUpper().ToArray();
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i] == 'L')
+                {
+                    if (playerPosition.Col - 1 < 0)
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        escapedSuccessfully = true;
+                    }
+                    else if (gameField[playerPosition.Row, playerPosition.Col - 1] == 'B')
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Col = playerPosition.Col - 1;
+                        isDead = true;
+                    }
+                    else if (gameField[playerPosition.Row, playerPosition.Col - 1] != 'B' && playerPosition.Col - 1 >= 0)
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col - 1] = 'P';
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Col = playerPosition.Col - 1;
+                    }
+                }
+                else if (commands[i] == 'R')
+                {
+                    if (playerPosition.Col + 1 > gameField.GetLength(1) - 1)
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        escapedSuccessfully = true;
+                    }
+                    else if (gameField[playerPosition.Row, playerPosition.Col + 1] == 'B')
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Col = playerPosition.Col + 1;
+                        isDead = true;
+                    }
+                    else if (gameField[playerPosition.Row, playerPosition.Col + 1] != 'B' && playerPosition.Col + 1 < gameField.GetLength(1))
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col + 1] = 'P';
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Col = playerPosition.Col + 1;
+                    }
+                }
+                else if (commands[i] == 'U')
+                {
+                    if (playerPosition.Row - 1 < 0)
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        escapedSuccessfully = true;
+                    }
+                    else if (gameField[playerPosition.Row - 1, playerPosition.Col] == 'B')
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Row = playerPosition.Row - 1;
+                        isDead = true;
+                    }
+                    else if (gameField[playerPosition.Row - 1, playerPosition.Col] != 'B' && playerPosition.Row - 1 >= 0)
+                    {
+                        gameField[playerPosition.Row - 1, playerPosition.Col] = 'P';
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Row = playerPosition.Row - 1;
+                    }
+                }
+                else if (commands[i] == 'D')
+                {
+                    if (playerPosition.Row + 1 > gameField.GetLength(0) - 1)
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        escapedSuccessfully = true;
+                    }
+                    else if (gameField[playerPosition.Row + 1, playerPosition.Col] == 'B')
+                    {
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Row = playerPosition.Row + 1;
+                        isDead = true;
+                    }
+                    else if (gameField[playerPosition.Row + 1, playerPosition.Col] != 'B' && playerPosition.Row + 1 >= 0)
+                    {
+                        gameField[playerPosition.Row + 1, playerPosition.Col] = 'P';
+                        gameField[playerPosition.Row, playerPosition.Col] = '.';
+                        playerPosition.Row = playerPosition.Row + 1;
+                    }
+                }
+
+                List<Position> rabbits = new List<Position>();
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (gameField[row, col] == 'B')
+                        {
+                            Position rabbit = new Position(row, col);
+                            rabbits.Add(rabbit);
+                        }
+                    }
+                }
+
+
+                for (int j = 0; j < rabbits.Count; j++)
+                {
+                    var row = rabbits[j].Row;
+                    var col = rabbits[j].Col;
+                    if (gameField[row, col] == 'B')
+                    {
+                        //up propagation
+                        if (row - 1 >= 0 && gameField[row - 1, col] == '.')
+                        {
+                            gameField[row - 1, col] = 'B';
+                        }
+                        else if (row - 1 >= 0 && gameField[row - 1, col] == 'P')
+                        {
+                            gameField[row - 1, col] = 'B';
+                            isDead = true;
+                        }
+                        //down propagation
+                        if (row + 1 <= gameField.GetLength(0) - 1 && gameField[row + 1, col] == '.')
+                        {
+                            gameField[row + 1, col] = 'B';
+                        }
+                        else if (row + 1 <= gameField.GetLength(0) - 1 && gameField[row + 1, col] == 'P')
+                        {
+                            gameField[row + 1, col] = 'B';
+                            isDead = true;
+                        }
+                        //right propagation
+                        if (col + 1 <= gameField.GetLength(1) - 1 && gameField[row, col + 1] == '.')
+                        {
+                            gameField[row, col + 1] = 'B';
+                        }
+                        else if (col + 1 <= gameField.GetLength(1) - 1 && gameField[row, col + 1] == 'P')
+                        {
+                            gameField[row, col + 1] = 'B';
+                            isDead = true;
+                        }
+                        //left propagation
+                        if (col - 1 >= 0 && gameField[row, col - 1] == '.')
+                        {
+                            gameField[row, col - 1] = 'B';
+                        }
+                        else if (col - 1 >= 0 && gameField[row, col - 1] == 'P')
+                        {
+                            gameField[row, col - 1] = 'B';
+                            isDead = true;
+                        }
+                    }
+                }
+
+
+                if (escapedSuccessfully)
+                {
+                    PrintGameField(gameField);
+                    Console.WriteLine($"won: {playerPosition.Row} {playerPosition.Col}");
+                    return;
+                }
+                if (isDead)
+                {
+                    PrintGameField(gameField);
+                    Console.WriteLine($"dead: {playerPosition.Row} {playerPosition.Col}");
+                    return;
+                }
+            }
+        }
+
+        public static char[,] GenerateGameField(char[,] gameField, int rows, int cols)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                char[] input = Console.ReadLine().ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    gameField[row, col] = input[col];
+                    if (input[col] == 'P')
+                    {
+                        //TODO playerPosition
+                    }
+                }
+            }
+
+            return gameField;
+        }
+
+        public static void PrintGameField(char[,] gameField)
+        {
+            for (int row = 0; row < gameField.GetLength(0); row++)
+            {
+                for (int col = 0; col < gameField.GetLength(1); col++)
+                {
+                    Console.Write(gameField[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class Position
+    {
+        public int Row;
+        public int Col;
+
+        public Position(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+    }
+}
