335ef59cb7f23b153c3738f6fa1f45f87d11d584
AnastasPetrenski <hloro83@gmail.com>
2020-09-20 21:03:46 +0300

    StackAndQueue

---
+++ Advance01StacksAndQueues/DecimalToBinaryConvertor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecimalToBinaryConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            Stack<int> binary = new Stack<int>();
+
+            while (number > 0)
+            {
+                binary.Push(number % 2);
+                number /= 2;
+            }
+
+            List<int> print = new List<int>(binary);
+            Console.WriteLine(string.Join("", print));
+        }
+    }
+}
