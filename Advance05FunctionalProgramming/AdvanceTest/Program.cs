3218fd87ea630cf765279a22fdbf1962207a8501
AnastasPetrenski <hloro83@gmail.com>
2020-10-04 11:12:15 +0300

    FunctionalPragramming

--- Advance05FunctionalProgramming/AdvanceTest/Program.cs
+++ Advance05FunctionalProgramming/AdvanceTest/Program.cs
@@ -8,13 +8,32 @@ namespace AdvanceTest
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            Action<int, Func<int, int>> b = PrintOperation;
+            Func<int, int> calculation = x => x * x;
+            int c = Operation(a, c => FunctionSum(a,c));
+            b(c, x => FunctionMultiply(x,c));
+            b(c, calculation);
+
+
             AnotherFunction(SomeFunction);
-            var x = SecondFunction(Function);
+            var x = SecondFunction(FunctionSum);
             Console.WriteLine(x);
             RunTheMethod(() => Method1("MyString1"));
 
-            Func<int, int, int> func = Function;
+            Func<int, int, int> func = FunctionSum;
             Console.WriteLine(func);
+        }
+
+        private static int Operation(int number, Func<int, int> operation)
+        {
+            return operation(number);
+        }
+
+        static void PrintOperation(int number, Func<int, int> operation)
+        {
+            int c = Operation(number, operation);
+            Console.WriteLine(c);
         }
 
         static void AnotherFunction(Action<int, string, bool> action)
@@ -29,16 +48,33 @@ namespace AdvanceTest
 
         public static bool SecondFunction(Func<int, int, int> func)
         {
-            Func<int, int, int> r = (a,b) => 5+5;
-            
-            bool boo = r.Equals(func) ? true : false;
+            int c = 5;
+            int d = 5;
+            Func<int, int, int> r = (a, b) => 5 + 5;
+            int a = 0;
+            int b = 0;
+            int csd = r(a, b);
+            Console.WriteLine(csd);
+            var result = (FunctionSubtract(c,d));
+            Console.WriteLine(result);
+            bool boo = csd == result ? true : false;
             return boo;
         }
 
-        public static int Function(int z, int x)
+        public static int FunctionSum(int z, int x)
         {
-            int result = 5 + 5;
-            return result;
+            //int result = 5 + 5;
+            return z + x;
+        }
+
+        public static int FunctionSubtract(int z, int x)
+        {
+            return z - x;
+        }
+
+        public static int FunctionMultiply(int z, int x)
+        {
+            return z * x;
         }
 
         public static int Method1(string mystring)
@@ -58,6 +94,6 @@ namespace AdvanceTest
             return true;
         }
 
-        
+
     }
 }
